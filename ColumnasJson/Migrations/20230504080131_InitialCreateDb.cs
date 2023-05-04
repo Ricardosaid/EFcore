64d3fd67e4496389bf7105d0d42169d451438889
Ricardo Said <ricardo@smartpaymx.com>
2023-05-04 02:03:57 -0600

    added migration

---
+++ ColumnasJson/Migrations/20230504080131_InitialCreateDb.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ColumnasJson.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ventas",
+                columns: table => new
+                {
+                    ventaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    total = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    customer = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ventas", x => x.ventaId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ventas");
+        }
+    }
+}

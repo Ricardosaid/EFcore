93d2a854607c9fc1fb4234ccbf41d3cf86d28f72
Ricardo Said <ricardo@smartpaymx.com>
2023-05-04 01:43:25 -0600

    Added classes that modeling json

--- ColumnasJson/Models/Venta.cs
+++ ColumnasJson/Models/Venta.cs
@@ -10,5 +10,25 @@ public class Venta
     public int ventaId { get; set; }
 
     public decimal total { get; set; }
+    public DataCustomer customer { get; set; }
+    
+}
+
+public class DataCustomer
+{
+    public string name { get; set; }
+    public Address address { get; set; }
+    
+}
+
+public class Address
+{
+    private string _calle;
+    private string _ciudad;
+    public Address(string street, string city)
+    {
+        _calle = street;
+        _ciudad = city;
+    }
     
 }
--- ColumnasJson/Models/VentaContext.cs
+++ ColumnasJson/Models/VentaContext.cs
@@ -7,6 +7,6 @@ public class VentaContext : DbContext
     //Al ser un proyecto de consola, inyectamos la cadena de conexion desde el OnConfiguring, si es desde un framework, lo hacemos desde el program
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer("");
+        optionsBuilder.UseSqlServer("Server=localhost,1433;Database=ColumnasJson;User Id=sa;Password=SMARTpay123#;TrustServerCertificate=true");
     }
 }
